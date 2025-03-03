2745bb17345566f092e7a9ad2026fb3a5e6caf44
aaqibraza92 <78475535+aaqibraza92@users.noreply.github.com>
2025-03-03 14:41:53 +0530

    u

--- src/auth/AuthContext.js
+++ src/auth/AuthContext.js
@@ -4,23 +4,23 @@ import { createContext, useContext, useState } from "react";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [token, setToken] = useState(sessionStorage.getItem('token' || null))
+    const [token, setToken] = useState(sessionStorage.getItem('token') || null)
 
     const login = (newToken) => {
         sessionStorage.setItem("token", newToken)
         setToken(newToken);
     }
 
-    const logout=()=>{
+    const logout = () => {
         sessionStorage.removeItem("token")
         setToken(null);
     }
 
-    return(
-        <AuthContext.Provider value={{token,login,logout}}>
-{children}
+    return (
+        <AuthContext.Provider value={{ token, login, logout }}>
+            {children}
         </AuthContext.Provider>
     )
-} 
+}
 
-export const useAuth=()=>useContext(AuthContext)
+export const useAuth = () => useContext(AuthContext)
--- src/pages/private/Login.js
+++ src/pages/private/Login.js
@@ -1,8 +1,23 @@
+import {  useNavigate } from "react-router-dom";
+import { useAuth } from "../../auth/AuthContext";
+
 const Login=()=>{
+
+    const {login}=useAuth()
+    const navigate = useNavigate();
+    const handleLogin =()=>{
+        login("asidfo12812093")
+        navigate("/my-account");
+    }
     return(
-        <h2>
-       My Account Page
+        <>
+          <h2>
+       Login Page
         </h2>
+
+        <button onClick={handleLogin }>Login</button>
+        </>
+      
     )
     }
     
